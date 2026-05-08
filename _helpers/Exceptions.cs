691f5f811f3f69b09899bfe1569aaabe12a7795d
jakelorenzesabal <jakelorenzecan.com>
2026-05-08 18:38:05 +0800

    fixing refreshtoken

--- _helpers/Exceptions.cs
+++ _helpers/Exceptions.cs
@@ -1,5 +1,11 @@
 namespace PowerGuardCoreApi._Helpers
 {
+    public class AppException : Exception
+    {
+        public AppException() : base() { }
+        public AppException(string message) : base(message) { }
+    }
+
     public class RoomInactiveException : Exception
     {
         public RoomInactiveException(string message) : base(message) { }
